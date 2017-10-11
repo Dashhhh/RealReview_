a52df6c2754e915dda879eff05dc6bc9255d8e0e
Dashhhh <devil1032@gmail.com>
2017-10-12 00:25:51 +0900

    commit Test

--- app/src/main/java/com/teamnova/ej/realreview/adapter/ShopAdd3_Adapter.java
+++ app/src/main/java/com/teamnova/ej/realreview/adapter/ShopAdd3_Adapter.java
@@ -17,6 +17,8 @@ import java.util.ArrayList;
 
 public class ShopAdd3_Adapter extends BaseAdapter {
     String TestVAR;
+    String TestVAR2;
+
 
     public ArrayList<ShopAdd3_SetData> data = new ArrayList<>();
     LayoutInflater inflater = null;
